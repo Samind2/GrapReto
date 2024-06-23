f32280addae58cbf51ee949dff5751fd4d3262f1
Samind2 <suphaneerangsirat@gmail.com>
2024-06-23 19:19:00 +0700

    23.6.67

---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="p-4 shadow-md">
+      <h1 className="text-2xl font-bold text-center">Grab Restaurant</h1>
+    </header>
+  );
+}
+
+export default Header;
